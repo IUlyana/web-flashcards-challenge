3074761cb7bd8b7f119030241c029e2b2da1dcdb
Ulyana <ulechka1996@icloud.com>
2022-06-30 23:24:30 +0300

    added page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const path = require('path');
 const morgan = require('morgan');
 const indexRouter = require('./routes/index.router');
+const questionRouter = require('./routes/question.router');
+
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+app.use('/question', questionRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Сервер шуршит на ${PORT}`);
